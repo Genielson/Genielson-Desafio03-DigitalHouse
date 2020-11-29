1d8ede0ca333a9f780eae7a5400448051d2d2d8f
Genielson <ge.nielson@hotmail.com>
2020-11-29 19:46:48 -0300

    Adicionando validacao de dados em login e register

--- app/src/main/java/com/example/desafio03digitalhouse/home/login/LoginFragment.kt
+++ app/src/main/java/com/example/desafio03digitalhouse/home/login/LoginFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 import com.example.desafio03digitalhouse.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class LoginFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +22,73 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        view.findViewById<Button>(R.id.containedButtonLogin).setOnClickListener {
+            if(validaDadosLogin(view)) {
+                navController.navigate(R.id.action_loginFragment_to_homeFragment)
+            }
+        }
+
+        view.findViewById<Button>(R.id.containedButtonRegister).setOnClickListener {
+            if(validaDadosRegister(view)) {
+                navController.navigate(R.id.action_loginFragment_to_registerFragment)
+            }
+        }
+
+    }
+
+    private fun validaDadosLogin(view:View):Boolean{
+
+        var email = view.findViewById<TextInputEditText>(R.id.txtFieldEmailLoginContent)
+
+        var password = view.findViewById<TextInputEditText>(R.id.txtFieldPasswordLoginContent)
+
+        var resultado = true
+
+        if(email.text?.isBlank()!!){
+            view.findViewById<TextInputLayout>(R.id.txtFieldEmailLogin).error = getString(R.string.empty_email)
+            resultado = false
+        }
+
+        if(password.text?.isBlank()!!){
+            view.findViewById<TextInputLayout>(R.id.txtFieldPasswordLogin).error = getString(R.string.empty_password)
+            resultado = false
+        }
+
+        return resultado
+    }
+
+    private fun validaDadosRegister(view:View):Boolean{
+
+        var name = view.findViewById<TextInputEditText>(R.id.txtFieldNameRegisterContent)
+
+        var email = view.findViewById<TextInputEditText>(R.id.txtFieldEmailRegisterContent)
+
+        var password = view.findViewById<TextInputEditText>(R.id.txtFieldPasswordRegisterContent)
+
+        var resultado = true
+
+        if(name.text?.isBlank()!!){
+            view.findViewById<TextInputLayout>(R.id.txtFieldNameRegister).error = getString(R.string.empty_name)
+            resultado = false
+        }
+
+        if(email.text?.isBlank()!!){
+            view.findViewById<TextInputLayout>(R.id.txtFieldEmailRegister).error = getString(R.string.empty_email)
+            resultado = false
+        }
+
+        if(password.text?.isBlank()!!){
+            view.findViewById<TextInputLayout>(R.id.txtFieldPasswordRegister).error = getString(R.string.empty_password)
+            resultado = false
+        }
+
+        return resultado
+
+    }
 
 }
